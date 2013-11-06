f171864029aa3b53ed2ad5ddc05479541409d8b7
woojj12 <woojj12@gmail.com>
2013-11-06 20:05:57 +0900

    Update loginspection.c

--- loginspection.c
+++ loginspection.c
@@ -1,179 +1,169 @@
-/*
- * loginspection.c
- *
- *  Created on: Nov 4, 2013
- *      Author: jungjae
- */
-
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <unistd.h>
 #include <assert.h>
 
-#define TRUE	1
-#define FALSE	0
-
+#define MAX_READ 1000
 #define MAX_BUFSIZE	1024
 
-typedef struct openedfile
+typedef struct page
 {
-//	char path[MAX_BUFSIZE];
+	int readid[MAX_READ];
+	int count;
+} PAGE;
+
+typedef struct file
+{
 	char *path;
-	long int size;
+	PAGE **page;
+	int size;
 
-	int *cow_page;
-	int pages;
-
-	struct openedfile *left, *right;
+	struct file *left, *right;
 } treeNode;
-treeNode *file_root;
+treeNode* file_root;
 
 int pagesize;
 
-void InsertLog();
-void MakeFileTree();
-treeNode * FindNode(treeNode *node, char* path);
-treeNode* FindMinNode(treeNode *node);
-treeNode* DeleteNode(treeNode *node, char* path);
-treeNode* InsertNode(treeNode *root, treeNode *node);
-treeNode* MakeNewNode(char* buf);
+void MakeInspection(void);
+void ReadHandle(char* buf);
+void StoreHandle(char* buf);
+treeNode* InsertNode(treeNode *node, char* path, int startpage, int endpage, int filesizepage);
+void Print(treeNode* node);
 
 int main(void)
 {
 	pagesize = getpagesize();
-
-	printf("makefiletree\n");
-//	MakeFileTree();
-
-	printf("insertlog\n");
-	InsertLog();
-
-	printf("printflog\n");
-	treeNode *node = FindMinNode(file_root);
-
-	while(node != NULL)
-	{
-		int offset;
-		printf("%s|%d|", node->path, node->pages);
-		for(offset = 0; offset < node->pages; offset++)
-		{
-			printf("%d|", node->cow_page[offset]);
-		}
-		printf("\n");
-		file_root = DeleteNode(file_root, node->path);
-		node = FindMinNode(file_root);
-	}
-
+	MakeInspection();
+	Print(file_root);
 	return 0;
 }
 
-void InsertLog()
+void Print(treeNode* node)
 {
-	FILE *fp = fopen("cowlog", "r");
-	assert(fp);
+	if(node == NULL)
+		return;
 
-	char buf[2][MAX_BUFSIZE];
-	treeNode *node;
+	Print(node->left);
 
-	while(fgets(buf[0], MAX_BUFSIZE, fp) > 0)
+	int i;
+	printf("%s|", node->path);
+
+	//cow
+	for(i=0; i<=node->size; i++)
 	{
-		int index = 0;
-
-		while(buf[0][index+5] != ':')
+		if(node->page[i] != NULL)
 		{
-			buf[1][index] = buf[0][index+5];
-			index++;
-		}
-		buf[1][index] = '\0';
-//		fprintf(stderr, "%s\n", buf[1]);
-
-		node = FindNode(file_root, buf[1]);
-		if(node == NULL)
-		{
-			node = (treeNode*)malloc(sizeof(treeNode));
-			assert(node);
-			node->path = strdup(buf[1]);
-			node->pages = 0;
-			node->cow_page = (int*)malloc(sizeof(int));
-			assert(node->cow_page);
-			node->cow_page[0] = FALSE;
-			node->size = 0;
-			node->left = NULL;
-			node->right = NULL;
-			file_root = InsertNode(file_root, node);
-		}
-		assert(node);
-
-		index = index + 6;
-
-		long int startoffset = 0;
-		long int endoffset = 0;
-
-		while(buf[0][index] != ':')
-		{
-			if(buf[0][index] >= 'a' && buf[0][index] <= 'z')
-				startoffset = startoffset*16 + (buf[0][index] - 'a');
+			if(node->page[i]->count != 0)
+				printf("%d|", node->page[i]->count);
 			else
-				startoffset = startoffset*16 + (buf[0][index] - '0');
-			index++;
+				printf("|");
 		}
-		index++;
-
-		while(buf[0][index] != ':' && buf[0][index] != '\n' && buf[0][index] != '\0')
-		{
-			if(buf[0][index] >= 'a' && buf[0][index] <= 'z')
-				endoffset = endoffset*16 + (buf[0][index] - 'a');
-			else
-				endoffset = endoffset*16 + (buf[0][index] - '0');
-			index++;
-		}
-
-		int pageoffset = startoffset / pagesize;
-		while(1)
-		{
-			if(pageoffset >= node->pages)
-			{
-				node->cow_page = (int*)realloc(node->cow_page, sizeof(int)*(pageoffset+1));
-				assert(node->cow_page);
-				memset(node->cow_page+node->pages+1, 0, pageoffset - node->pages);
-				node->pages = pageoffset;
-			}
-
-			assert(node->cow_page);
-//			fprintf(stderr, "%s %d %d\n", node->path, node->pages, pageoffset);
-			node->cow_page[pageoffset] = TRUE;
-			pageoffset = pageoffset + 1;
-			if(pageoffset * pagesize > endoffset)
-				break;
-		}
+		else
+			printf("|");
 	}
+	printf("\n|");
 
-	fclose(fp);
+	//read
+	for(i=0; i<=node->size; i++)
+	{
+		if(node->page[i] != NULL)
+		{
+			printf("r|");
+		}
+		else
+			printf("|");
+	}
+	printf("\n|");
+
+	//file
+	for(i=0; i<=node->size; i++)
+	{
+		printf("|");
+	}
+	printf("\n");
+
+	Print(node->right);
 }
 
-void MakeFileTree()
+void MakeInspection()
 {
-	FILE *fp = fopen("openlog", "r");
+	FILE *fp = fopen("log", "r");
 	assert(fp);
 
 	char buf[MAX_BUFSIZE];
-	treeNode *node;
 
 	while(fgets(buf, MAX_BUFSIZE, fp) > 0)
 	{
-		node = MakeNewNode(buf);
-//		fprintf(stderr, "%s\n", node->path);
-//		file_root = DeleteNode(file_root, node->path);
-		file_root = InsertNode(file_root, node);
+		if(buf[0] == '/')
+		{
+			//Read
+			ReadHandle(buf);
+		}
+		else
+		{
+			//Store
+			StoreHandle(buf);
+		}
 	}
-
 	fclose(fp);
 }
 
-treeNode * FindNode(treeNode *node, char* path)
+void ReadHandle(char* buf)
+{
+	int i = 0;
+	char path[MAX_BUFSIZE];
+	long unsigned int offset, readsize, filesize;
+	int startpage, endpage, filesizepage;
+
+	offset = 0;
+	readsize = 0;
+	filesize = 0;
+
+	while(buf[i] != ':')
+	{
+		path[i] = buf[i];
+		i++;
+	}
+	path[i] = '\0';
+	i++;
+
+	while(buf[i] != ':')
+	{
+		if(buf[i] >= 'a' && buf[i] <= 'z')
+			offset = offset*16 + (buf[i] - 'a');
+		else
+			offset = offset*16 + (buf[i] - '0');
+		i++;
+	}
+	i++;
+
+	while(buf[i] != ':')
+	{
+		if(buf[i] >= 'a' && buf[i] <= 'z')
+			readsize = readsize*16 + (buf[i] - 'a');
+		else
+			readsize = readsize*16 + (buf[i] - '0');
+		i++;
+	}
+	i++;
+
+	while(buf[i] != ':')
+	{
+		if(buf[i] >= 'a' && buf[i] <= 'z')
+			filesize = filesize*16 + (buf[i] - 'a');
+		else
+			filesize = filesize*16 + (buf[i] - '0');
+		i++;
+	}
+
+	startpage = offset / pagesize;
+	endpage = (offset+readsize) / pagesize;
+	filesizepage = filesize / pagesize;
+
+	file_root = InsertNode(file_root, path, startpage, endpage, filesizepage);
+}
+
+treeNode* FindNode(treeNode *node, char* path)
 {
 	if(node==NULL)
 	{
@@ -200,135 +190,143 @@ treeNode * FindNode(treeNode *node, char* path)
 	}
 }
 
-treeNode* FindMinNode(treeNode *node)
-{
-	if(node==NULL)
-	{
-		/* There is no element in the tree */
-		return NULL;
-	}
-	if(node->left) /* Go to the left sub tree to find the min element */
-		return FindMinNode(node->left);
-	else
-		return node;
-}
-
-treeNode* DeleteNode(treeNode *node, char* path)
+treeNode* InsertNode(treeNode *node, char* path, int startpage, int endpage, int filesizepage)
 {
 	if(node == NULL)
-		return NULL;
-	int cmp = strcmp(node->path, path);
-	if(cmp > 0)
 	{
-		node->left = DeleteNode(node->left, path);
-	}
-	else if(cmp < 0)
-	{
-		node->right = DeleteNode(node->right, path);
+		node = (treeNode*)malloc(sizeof(treeNode));
+		assert(node);
+		node->path = strdup(path);
+		node->size = filesizepage;
+		node->page = (PAGE**)malloc(sizeof(PAGE*)*(filesizepage+1));
+		assert(node->page);
+		memset(node->page, 0, sizeof(PAGE*)*(filesizepage+1));
+		node->left = NULL;
+		node->right = NULL;
+
+		int i;
+		for(i=startpage; i<=endpage; i++)
+		{
+			node->page[i] = (PAGE*)malloc(sizeof(PAGE));
+			assert(node->page[i]);
+			node->page[i]->count = 0;
+		}
 	}
 	else
 	{
-		/* Now We can delete this node and replace with either minimum element
-                   in the right sub tree or maximum element in the left subtree */
-		treeNode *temp;
-		if(node->right && node->left)
-		{
-			/* Here we will replace with minimum element in the right sub tree */
-			temp = FindMinNode(node->right);
-
-//			if(node->cow_page != NULL)
-//				free(node->cow_page);
-			node->cow_page = temp->cow_page;
-//			temp->cow_page = NULL;
-//			if(node->path != NULL)
-//				free(node->path);
-			node->path = temp->path;
-//			temp->path = NULL;
-
-			node->size = temp->size;
-
-			/* As we replaced it with some other node, we have to delete that node */
-			node -> right = DeleteNode(node->right,node->path);
-		}
-		else
-		{
-			/* If there is only one or zero children then we can directly
-                           remove it from the tree and connect its parent to its child */
-			temp = node;
-			if(node->left == NULL)
-				node = node->right;
-			else if(node->right == NULL)
-				node = node->left;
-			else
-				assert(0);
-//			if(temp->path != NULL)
-//				free(temp->path);
-//			if(temp->cow_page != NULL)
-//				free(temp->cow_page);
-			free(temp); /* temp is longer required */
-		}
-	}
-	return node;
-}
-
-treeNode* InsertNode(treeNode *root, treeNode *node)
-{
-	if(root == NULL)
-		return node;
-	else
-	{
-		int cmp = strcmp(root->path, node->path);
+		int cmp = strcmp(node->path, path);
 		if(cmp > 0)
-			root->left = InsertNode(root->left, node);
+			node->left = InsertNode(node->left, path, startpage, endpage, filesizepage);
 		else if(cmp < 0)
-			root->right = InsertNode(root->right, node);
+			node->right = InsertNode(node->right, path, startpage, endpage, filesizepage);
 		else
 		{
-			free(node->cow_page);
-			free(node->path);
-			free(node);
+			if(node->size < filesizepage)
+			{
+				PAGE** tmp = (PAGE**)malloc(sizeof(PAGE*)*(filesizepage+1));
+				memset(tmp, 0, sizeof(PAGE*)*(filesizepage+1));
+				memcpy(tmp, node->page, node->size+1);
+				free(node->page);
+				node->page = tmp;
+				node->size = filesizepage;
+			}
+			int i;
+			for(i=startpage; i<=endpage; i++)
+			{
+				if(node->page[i] == NULL)
+				{
+					node->page[i] = (PAGE*)malloc(sizeof(PAGE));
+					assert(node->page[i]);
+					node->page[i]->count = 0;
+				}
+			}
 		}
-
-		return root;
 	}
+	return node;
 }
 
-treeNode* MakeNewNode(char* buf)
+void StoreHandle(char* buf)
 {
-	int index;
-	int count;
 	char path[MAX_BUFSIZE];
-	treeNode *node = (treeNode*)malloc(sizeof(treeNode));
-	if(node == NULL)
-		exit(-1);
+	int i = 0;
+	int j = 0;
+	int readid = 0;
 
-	index = 0;
-	while(buf[index+2] != '|')
+	long unsigned int startoffset = 0, endoffset = 0;
+
+	while(buf[i] != ':')
 	{
-		path[index] = buf[index+2];
-		index++;
-	}
-	path[index] = '\0';
-
-	node->path = strdup(path);
-
-	index = index + 3;
-
-	node->size = 0;
-	while(buf[index] != '|')
-	{
-		if(buf[index] >= 'a' && buf[index] <= 'z')
-			node->size = node->size*16 + (buf[index] - 'a');
+		if(buf[i] >= 'a' && buf[i] <= 'z')
+			readid = readid*16 + (buf[i] - 'a');
 		else
-			node->size = node->size*16 + (buf[index] - '0');
-		index++;
+			readid = readid*16 + (buf[i] - '0');
+		i++;
 	}
+	i++;
 
-	node->pages = node->size / pagesize;
-	node->cow_page = (int*)calloc(node->pages+1, sizeof(int));
+	while(buf[i] != ':')
+	{
+		path[j] = buf[i];
+		i++;
+		j++;
+	}
+	path[j] = '\0';
+	i++;
 
-	node->left = NULL;
-	node->right = NULL;
+	while(buf[i] != ':')
+	{
+		if(buf[i] >= 'a' && buf[i] <= 'z')
+			readid = readid*16 + (buf[i] - 'a');
+		else
+			readid = readid*16 + (buf[i] - '0');
+		i++;
+	}
+	i++;
 
-	return node;
+	while(buf[i] != ':')
+	{
+		if(buf[i] >= 'a' && buf[i] <= 'z')
+			startoffset = startoffset*16 + (buf[i] - 'a');
+		else
+			startoffset = startoffset*16 + (buf[i] - '0');
+		i++;
+	}
+	i++;
+
+	while(buf[i] != ':')
+	{
+		if(buf[i] >= 'a' && buf[i] <= 'z')
+			endoffset = endoffset*16 + (buf[i] - 'a');
+		else
+			endoffset = endoffset*16 + (buf[i] - '0');
+		i++;
+	}
+	i++;
+
+	treeNode *file = FindNode(file_root, path);
+	assert(file);
+
+	int startpage = startoffset / pagesize;
+	int endpage = endoffset / pagesize;
+
+	if(file->size < endpage)
+	{
+		endpage = file->size;
+	}
+	i = startpage;
+
+	NEXTLOOP:
+	for(; i<=endpage; i++)
+	{
+		for(j=0; j<file->page[i]->count; j++)
+		{
+			if(file->page[i]->readid[j] == readid)
+			{
+				i++;
+				goto NEXTLOOP;
+			}
+		}
+		file->page[i]->readid[file->page[i]->count] = readid;
+		file->page[i]->count++;
+	}
 }
